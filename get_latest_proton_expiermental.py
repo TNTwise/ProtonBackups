0d89e92d621f0946349456ec0b31bb82c7e67c55
Paxson Gottschalk <tntwise3@gmail.com>
2022-10-29 12:50:44 -0500

    modified:   get_latest_proton_expiermental.py

--- get_latest_proton_expiermental.py
+++ get_latest_proton_expiermental.py
@@ -1,15 +1,11 @@
-# IMPORTANT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# You need to be on the latest bleeding edge version of proton for it to extract the right version
-
-
-
+#!/usr/bin/python
 import os
 import csv
 from posixpath import expanduser
 import re
 home_dir = expanduser("~")
 
-# This code extracts the build version of proton into the Downloads folder
+# This code extracts the build version of proton
 
 f = open(home_dir+"/.local/share/Steam/steamapps/common/Proton - Experimental/version", "r")
 f = csv.reader(f)
@@ -26,14 +22,14 @@ proton_ver = "ExBE" + proton_ver
 proton_dir = home_dir+'/Downloads/'+proton_ver+'/'+proton_ver
 
 os.system("mkdir /home/$USER/Downloads/" + proton_ver)
-os.system('cp -r "/home/$USER/.local/share/Steam/steamapps/common/Proton - Experimental/" /home/$USER/Downloads/' + proton_ver)
-os.system('mv "/home/$USER/Downloads/'+proton_ver+'/Proton - Experimental/" /home/$USER/Downloads/'+proton_ver+'/'+proton_ver)
+os.system('cp -r /home/$USER/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/* /home/$USER/Downloads/' + proton_ver+'/')
+#os.system('mv "/home/$USER/Downloads/'+proton_ver+'/Proton - Experimental/" /home/$USER/Downloads/'+proton_ver+'/'+proton_ver)
 
 
 # This code changes the values within the files to match the proton version.
 temp_list = []
 
-with open(home_dir+'/Downloads/'+proton_ver+'/'+proton_ver+"/compatibilitytool.vdf", "w") as f:
+with open(home_dir+'/Downloads/'+proton_ver+'/'+"/compatibilitytool.vdf", "w") as f:
     f.write('"compatibilitytools"\n')
     f.write("{\n")
     f.write('  "compat_tools"\n')
@@ -52,7 +48,7 @@ temp_list = []
 
 # this will change the proton previx version within the proton file
 
-with open(home_dir+'/Downloads/'+proton_ver+'/'+proton_ver+"/proton", "r") as f:
+with open(home_dir+'/Downloads/'+proton_ver+'/'+"/proton", "r") as f:
     for line in f:
         temp_list.append(line) # Creates a list of all the lines within the proton file.
 
@@ -60,6 +56,6 @@ temp_list[42] = 'CURRENT_PREFIX_VERSION="'+proton_ver+'"'
 
 # This will write the changes to the file
 
-with open(home_dir+'/Downloads/'+proton_ver+'/'+proton_ver+"/proton", "w") as f:
+with open(home_dir+'/Downloads/'+proton_ver+'/'+"/proton", "w") as f:
     for value in temp_list:
         f.write(value)
