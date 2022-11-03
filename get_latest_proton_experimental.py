f0c141db5003f11376d05e3f94f91d3be75a68fc
Paxson Gottschalk <tntwise3@gmail.com>
2022-11-03 17:19:45 -0500

    new file:   get_latest_proton_experimental.py

---
+++ get_latest_proton_experimental.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+
+#IMPORTANT !!!!!!!!!!!!!!!!!!!
+# You need to be enrolled in the proton expiermental beta for this to work, otherwise it will just get the latest expiermental non-bleeding edge
+
+from genericpath import isfile
+import os
+import csv
+from posixpath import expanduser
+import re
+home_dir = expanduser("~")
+
+# This code extracts the build version of proton
+
+f = open(home_dir+"/.local/share/Steam/steamapps/common/Proton - Experimental/version", "r")
+f = csv.reader(f)
+for row in f: 
+    build_num = row
+proton_ver = ""
+for value in row:
+    proton_ver = value
+proton_ver = re.findall("-[\d].[\d]-[\d]*", proton_ver)
+for value in proton_ver:
+    proton_ver = value
+
+proton_ver = "ExBE" + proton_ver
+proton_dir = home_dir+'/Downloads/'+proton_ver+'/'+proton_ver
+
+#Checks to see if the proton version has already been copied, this prevents errors.
+if os.path.isfile(home_dir+'/Downloads/' + proton_ver+"/proton") == True:
+    print("This version has already been extracted.")
+else:
+    os.system("mkdir /home/$USER/Downloads/" + proton_ver)
+    os.system('cp -r /home/$USER/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/* /home/$USER/Downloads/' + proton_ver+'/')
+
+
+    # This code changes the values within the files to match the proton version.
+    temp_list = []
+
+    with open(home_dir+'/Downloads/'+proton_ver+'/'+"/compatibilitytool.vdf", "w") as f:
+        f.write('"compatibilitytools"\n')
+        f.write("{\n")
+        f.write('  "compat_tools"\n')
+        f.write('  {\n')
+        f.write('    "'+proton_ver +'"'+' // Internal name of this tool  \n')
+        f.write('    {\n')
+        f.write('      "install_path" "."\n')
+        f.write('      "display_name"' +' "'+proton_ver+'"\n')
+        f.write('      "from_oslist"  "windows"\n')
+        f.write('      "to_oslist"    "linux"\n')
+        f.write('    }\n')
+        f.write('  }\n')
+        f.write('}\n')
+
+    temp_list = []
+
+    # this will change the proton previx version within the proton file
+
+    with open(home_dir+'/Downloads/'+proton_ver+'/'+"/proton", "r") as f:
+        for line in f:
+            temp_list.append(line) # Creates a list of all the lines within the proton file.
+
+    temp_list[42] = 'CURRENT_PREFIX_VERSION="'+proton_ver+'"'
+
+    # This will write the changes to the file
+
+    with open(home_dir+'/Downloads/'+proton_ver+'/'+"/proton", "w") as f:
+        for value in temp_list:
+            f.write(value)
+
+#this code will ask the user if they want to zip it
+#This is dumb
+print("Extracted Successfuly.")
+isCompress = input("Do you want to compress it? (y / n): ")
+isCompress = isCompress.lower()
+if isCompress == "y":
+    if os.path.isfile(home_dir+'/Downloads/'+proton_ver+'.zip') == False:
+        os.chdir(home_dir+'/Downloads')
+        os.system('zip  -b '+home_dir+'/Downloads/ -r -y '+proton_ver+'.zip  '+proton_ver+'/*')
+    else:
+        print("You have already compressed this version.")
+
+    # Asks the user if they would like to automatically move it to the compat folder.
+    
+isMove = input("Would you like to move the extracted version to the 'compatibilitytools.d' folder? (y / n): ")
+isMove = isMove.lower()
+if isMove == "y":
+    if os.path.isfile(home_dir+"/.steam/steam/compatibilitytools.d/"+proton_ver+"/proton") == False:
+            os.system("mv "+home_dir+"/Downloads/"+proton_ver+" "+home_dir+"/.steam/steam/compatibilitytools.d/")
+    else:
+        print("The version you have extracted already exists in the compatibilitytools.d folder.")
